7fc28bd4c572e0f977d539964924898fd4011d4b
Dave Wiard <dave.wiard@gmail.com>
2018-06-29 06:40:50 -0700

    Base Angular project for My Stock Portfolio

---
+++ msp-angular/src/app/all-trades/all-trades.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-trades',
+  templateUrl: './all-trades.component.html',
+  styleUrls: ['./all-trades.component.scss']
+})
+export class AllTradesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
