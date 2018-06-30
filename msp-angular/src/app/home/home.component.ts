ef4abdfca37f5dcd8d36457729967ac86609c6a1
Dave Wiard <dave.wiard@gmail.com>
2018-06-29 21:48:23 -0700

    Changed JSON document structure
    Changed JSON document structure to match what will be stored in MongoDB.

--- msp-angular/src/app/home/home.component.ts
+++ msp-angular/src/app/home/home.component.ts
@@ -6,15 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  todaysReturn = '150.00';
-  totalReturn = '1500.00';
-  totalEquity = '9000.00';
+  todaysReturn = '34.39';
+  totalReturn = '2009.27';
+  totalEquity = '9341.28';
 
   stocks = [
     {
-      symbol: 'AIEQ',
+      symbol_info: {
+        symbol: 'AIEQ',
+        name: 'AI Powered Equity',
+        broker: 'Robinhood',
+      },
       shares: '7.0000',
-      name: 'AI Powered Equity',
       price: '27.92',
       todaysChange: '0.12',
       todaysReturn: '1.04',
@@ -22,9 +25,12 @@ export class HomeComponent implements OnInit {
       totalReturn: '2.20',
     },
     {
-      symbol: 'FDIS',
+      symbol_info: {
+        symbol: 'FDIS',
+        name: 'Fidelity MSCI Consumer Discretionary ETF',
+        broker: 'Robinhood',
+      },
       shares: '4.0000',
-      name: 'Fidelity MSCI Consumer Discretionary ETF',
       price: '27.92',
       todaysChange: '0.12',
       todaysReturn: '1.04',
@@ -32,9 +38,12 @@ export class HomeComponent implements OnInit {
       totalReturn: '2.20',
     },
     {
-      symbol: 'FIS',
+      symbol_info: {
+        symbol: 'FIS',
+        name: 'Fidelity National Information Services, Inc.',
+        broker: 'Fidelity',
+      },
       shares: '52.9800',
-      name: 'Fidelity National Information Services, Inc.',
       price: '27.92',
       todaysChange: '0.12',
       todaysReturn: '1.04',
@@ -42,9 +51,12 @@ export class HomeComponent implements OnInit {
       totalReturn: '2.20',
     },
     {
-      symbol: 'VPU',
+      symbol_info: {
+        symbol: 'VPU',
+        name: 'Vanguard Utilities ETF',
+        broker: 'Robinhood',
+      },
       shares: '2.0000',
-      name: 'Vanguard Utilities ETF',
       price: '27.92',
       todaysChange: '0.12',
       todaysReturn: '-1.04',
@@ -52,9 +64,12 @@ export class HomeComponent implements OnInit {
       totalReturn: '2.20',
     },
     {
-      symbol: 'XLK',
+      symbol_info: {
+        symbol: 'XLK',
+        name: 'Technology Select Sector SPDR',
+        broker: 'Robinhood',
+      },
       shares: '3.0000',
-      name: 'Technology Select Sector SPDR',
       price: '69.48',
       todaysChange: '0.12',
       todaysReturn: '1.04',
